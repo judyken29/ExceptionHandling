7d0e8fc3940719788a3e796de053905b3efc0e89
judyken29 <81622421+judyken29@users.noreply.github.com>
2021-04-15 16:27:58 +0530

    Update EmployeeService.java

--- EmployeeService.java
+++ EmployeeService.java
@@ -1,28 +1,42 @@
 package p2;
 
-public class EmployeeService {
-	private String employees[] = {"1010010","102001","1030020","1040030","1050020","1060020"};
+public class EmployeeService 
+{
+	private String employees[] = {"101001011","102001022","103002011","104003080","105002020","106002080"};
+	/*
+	 * Note : employee codes are designed in following way: 
+	 * a) first 3 digits are employee code
+	 * b) next 3 digits are related to their department id.
+	 * c) and last 3 digits are the city code of their work location 
+	 *     for example 
+	 *       011 : Delhi 
+	 *       022 : Mumbai 
+	 *       080 : Banglore
+	 *       020 : Pune
+	 * */
 	
-
 	public String[] getEmployeeCodes()
 	{
 		String codes[] = new String[employees.length];
 		int count = -1;
-		for (String empid : employees) {
+		for (String empid : employees) 
+		{
 			codes[++count] = empid.substring(0, 3);
 		}
 		
 		return codes;
 	}
-	
 	public String[] getCityCodes()
 	{
 		String codes[] = new String[employees.length];
 		int count = -1;
-		for (String empid : employees) {
+		for (String empid : employees) 
+		{
 			codes[++count] = empid.substring(6,9);
 		}
 		
 		return codes;
 	}
+
+
 }
